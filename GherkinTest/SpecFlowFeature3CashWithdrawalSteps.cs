99dbe76da047f4b9b19c55949481c812ec52ebb3
DavidH99 <david.hollander@student.hu.nl>
2020-09-01 16:51:34 +0200

    Further development on practice feature3

--- GherkinTest/SpecFlowFeature3CashWithdrawalSteps.cs
+++ GherkinTest/SpecFlowFeature3CashWithdrawalSteps.cs
@@ -1,23 +1,49 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using TechTalk.SpecFlow;
 
 namespace GherkinTest
 {
+    class ATM
+    {
+        int balance;
+    }
+    class Account
+    {
+        int balance;
+
+        public void deposit(int amount)
+        {
+            this.balance += amount;
+        }
+        public void withdraw(int amount)
+        {
+            this.balance -= amount;
+        }
+        public int getBalance()
+        {
+            return this.balance;
+        }
+    }
+
     [Binding]
     public class SpecFlowFeature3CashWithdrawalSteps
     {
+
         [Given(@"I have deposited \$(.*) in my account")]
         public void GivenIHaveDepositedInMyAccount(int p0)
         {
-            ScenarioContext.Current.Pending();
+            Account my_account = new Account();
+            my_account.deposit(p0);
+            my_account.getBalance().Should().Be(p0);
         }
         
         [When(@"I request \$(.*)")]
         public void WhenIRequest(int p0)
         {
-            ScenarioContext.Current.Pending();
+            ATM 
         }
-        
+
         [Then(@"\$(.*) should be dispensed")]
         public void ThenShouldBeDispensed(int p0)
         {
