5c676b9e369ae60071b32a8b0d050bd8ba921296
DavidH99 <david.hollander@student.hu.nl>
2020-09-07 17:13:13 +0200

    Added begin to feature 5

--- GherkinTest/SpecFlowFeature5ResponseInDBSteps.cs
+++ GherkinTest/SpecFlowFeature5ResponseInDBSteps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GherkinTest.Feature_5;
+using RestSharp;
 using TechTalk.SpecFlow;
 
 namespace GherkinTest
@@ -10,19 +11,35 @@ namespace GherkinTest
         [Given(@"there is a hotel with hotelID (.*)")]
         public void GivenThereIsAHotelWithHotelID(string p0)
         {
-            ScenarioContext.Current.Pending();
         }
         
         [When(@"there is a reservation with ExternalID(.*)")]
         public void WhenThereIsAReservationWithExternalID(string p0)
         {
-            ScenarioContext.Current.Pending();
+            // push to basher to invoke request for ota 'exp'
         }
         
         [Then(@"I expect a response in the DB")]
         public void ThenIExpectAResponseInTheDB()
         {
-            ScenarioContext.Current.Pending();
+            // do the verification call
+            GetReservationsFromBasher();
+        }
+
+        private void GetReservationsFromBasher()
+        {
+            var baseurl = "https://acceptance.smarthotel.nl/basher/api/";
+            baseurl = "https://basherweb20200903162540.azurewebsites.net/api/";
+            IRestClient client = new RestClient(baseurl);
+
+            IRestRequest request = new RestRequest("/api/reservations", Method.GET, DataFormat.Json);
+            request.AddParameter("hotelid", 42075, ParameterType.QueryString);
+            request.AddParameter("whitelabelid", 1020, ParameterType.QueryString);
+
+            IRestResponse response = client.Get(request);
+
+            var statuscode = response.StatusCode;
+            var content = response.Content;
         }
     }
 }
